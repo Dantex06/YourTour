85f2f30412a25e4a4564e3e613a1ac61d8b9bf4b
Dantex06 <egorrogacev030@gmail.com>
2025-04-28 23:35:20 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+const solve = (a,b ) => {
+  console.log(a + b);
+}
+
+solve(5, 6);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('node:path');
+
+const mode = process.env.NODE_ENV || 'development';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src', 'index.html'),
+  }),],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ]
+  }
+}
